f6ae0be5aa06def0149a1bfbc97d08c4ce5b81b3
Carlos Orelhas <c.orelhas@gmail.com>
2021-01-22 23:41:04 +0000

    Add mongodb connection

--- backend/server.js
+++ backend/server.js
@@ -2,11 +2,11 @@ import express from 'express';
 import dotenv from 'dotenv';
 import colors from 'colors';
 import morgan from 'morgan';
-// import connectDB from './config/db.js'
+import connectDB from './config/db.js';
 
 dotenv.config();
 
-// connectDB();
+connectDB();
 
 const app = express();
 
@@ -14,7 +14,9 @@ if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
 
-app.get('/', (req, res) => { res.send('API OK..')})
+app.get('/', (req, res) => {
+  res.send('API OK..');
+});
 
 app.use(express.json());
 
@@ -22,5 +24,7 @@ const PORT = process.env.PORT || 5000;
 
 app.listen(
   PORT,
-  console.log(`Server Running in ${process.env.NODE_ENV} mode on ${PORT}`)
+  console.log(
+    `Server Running in ${process.env.NODE_ENV} mode on ${PORT}`.yellow.bold
+  )
 );
