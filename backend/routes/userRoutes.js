2b93805f3fcf72cdc707b39cbca10ba755a0a26b
Carlos Orelhas <c.orelhas@gmail.com>
2021-02-08 22:12:34 +0000

    Register, login, getusers, delete,lete, update users backend

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,7 +1,30 @@
 import express from 'express';
 const router = express.Router();
-import { authUser } from '../controllers/userController.js';
+import {
+  authUser,
+  getUserProfile,
+  registerUser,
+  updateUserProfile,
+  deleteUser,
+  getUserById,
+  updateUser,
+  getUsers,
+} from '../controllers/userController.js';
+import { protect, admin } from '../middleware/authMiddleWare.js';
+
+router.route('/').post(registerUser).get(protect, admin, getUsers);
 
 router.route('/login', authUser);
+
+router
+  .route('/profile')
+  .get(protect, getUserProfile)
+  .put(protect, updateUserProfile);
+
+router
+  .route('/:id')
+  .delete(protect, admin, deleteUser)
+  .get(protect, admin, getUserById)
+  .put(protect, admin, updateUser);
 
 export default router;
